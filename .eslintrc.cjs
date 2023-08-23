5f6962ce9f3b9f370ad58ca7b6efd2a88c72f342
yhfu <yhfu@stu.xidian.edu.cn>
2023-08-24 01:13:36 +0800

    feat: 添加配置文件

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,18 +1,41 @@
 module.exports = {
-  root: true,
-  env: { browser: true, es2020: true },
-  extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
-  ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
-  rules: {
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
+    env: {
+        browser: true,
+        es6: true,
+        node: true,
+        jest: true
+    },
+    settings: {
+        react: {
+            version: "detect"
+        }
+    },
+    extends: [
+        'eslint:recommended',
+        'plugin:react/recommended',
+        'plugin:@typescript-eslint/recommended',
+        // 接入 prettier 的规则
+        'prettier',
+        'plugin:prettier/recommended'
     ],
-  },
-}
+    overrides: [],
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module'
+    },
+    plugins: ['react', '@typescript-eslint', 'prettier'],
+    rules: {
+        // 注意要加上这一句，开启 prettier 自动修复的功能
+        'prettier/prettier': 'error',
+        'quotes': ['error', 'single'],
+        'semi': ['error', 'always'],
+        'react/react-in-jsx-scope': 'off',
+        '@typescript-eslint/no-explicit-any': 'off',
+        '@typescript-eslint/no-empty-function': 'off',
+        'no-empty-pattern': 'off',
+        'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+        'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+        '@typescript-eslint/consistent-type-imports': 'error',
+    }
+};
